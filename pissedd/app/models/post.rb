f36f0d3ed3f73b0a0b61aca7c96dc4a0ffdb9def
Cuslar <cuslar@miuandes.cl>
2019-04-12 21:44:58 -0400

    avance

--- pissedd/app/models/post.rb
+++ pissedd/app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :description, length: {in 15..400}
+  validates :city, format: {with: /\A[a-zA-Z]+\z/}
+  validates :country, format: {with: /\A[a-zA-Z]+\z/}
+  validates: :site, :description, :date, :city, :country, :gps_location, :open, :solved, :innapropiated_count, :in_dumpster, :is_hidden, presence: true
 end
--- pissedd/app/models/user.rb
+++ pissedd/app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   belongs_to :zone
+  validates :user_type, inclusion: {in: %w(User Administrator Super_administrator)} 
+  validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :password, length: {in: 8..12} 
+  validates :password, format: { with: /\A[a-z0-9A-Z]+\z/,
+    message: "only allows alphanumeric characters" }
+  validates :bio, length: {in: 15..80}
+  validates :city, format: {with: /\A[a-zA-Z]+\z/}
+  validates :country, format: {with: /\A[a-zA-Z]+\z/}
+  validates :user_type, :email_address, :password, :city, :country, :in_blacklist, :suspended, :previously_suspension, presence: true
+  validates :email_address, uniqueness: true
+
+  
 end
